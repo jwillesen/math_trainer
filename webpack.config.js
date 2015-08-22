114d5d6a9d1a741a366d9e2f4833d80110459c31
jwillesen <git@wizardwell.net>
2015-08-22 09:31:19 -0600

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path')
+
+module.exports = {
+  // context: __dirname,
+  entry: './webpack/assets/javascripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: 'public/assets/javascripts',
+  },
+  resolve: {
+    root: [path.join(__dirname, 'webpack/assets/javascripts')],
+    extensions: ['', '.js', '.jsx', '.json'],
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          optional: ['runtime', 'es7.asyncFunctions', 'es7.objectRestSpread'],
+        },
+      },
+
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+
+      // make sure bootstrap can find jquery
+      {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery'},
+
+      // needed to load bootstrap's css
+      {test: /\.woff2?$/, loader: 'file', query: {name: '../../assets/fonts/[hash].[ext]'}},
+      {test: /\.ttf$/, loader: 'file', query: {name: '../../assets/fonts/[hash].[ext]'}},
+      {test: /\.eot$/, loader: 'file', query: {name: '../../assets/fonts/[hash].[ext]'}},
+      {test: /\.svg$/, loader: 'file', query: {name: '../../assets/fonts/[hash].[ext]'}},
+    ],
+  },
+}
---
+++ webpack/assets/javascripts/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+document.addEventListener('DOMContentLoaded', () => {
+  React.render(
+    <h1>Welcome to Math Trainer</h1>,
+    document.body)
+})
